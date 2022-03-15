e49e6fa4028e8fd8a4d6357357993dff0be94a02
A B M Zubayer <77219792+zubayer-47@users.noreply.github.com>
2022-03-15 13:17:29 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,151 @@
+[
+    {
+        idMeal: '1',
+        strMeal: 'Beef',
+        strMealThumb: 'https://www.themealdb.com/images/category/beef.png',
+        price: '100'
+    },
+    {
+        idMeal: '2',
+        strMeal: 'Chicken',
+        strMealThumb: 'https://www.themealdb.com/images/category/chicken.png',
+        price: '100'
+    },
+    {
+        idMeal: '3',
+        strMeal: 'Dessert',
+        strMealThumb: 'https://www.themealdb.com/images/category/dessert.png',
+        price: '100'
+    },
+    {
+        idMeal: '4',
+        strMeal: 'Lamb',
+        strMealThumb: 'https://www.themealdb.com/images/category/lamb.png',
+        price: '100'
+    },
+    {
+        idMeal: '5',
+        strMeal: 'Miscellaneous',
+        strMealThumb: 'https://www.themealdb.com/images/category/miscellaneous.png',
+        price: '100'
+    },
+    {
+        idMeal: '6',
+        strMeal: 'Pasta',
+        strMealThumb: 'https://www.themealdb.com/images/category/pasta.png',
+        price: '100'
+    },
+    {
+        idMeal: '7',
+        strMeal: 'Pork',
+        strMealThumb: 'https://www.themealdb.com/images/category/pork.png',
+        price: '100'
+    },
+    {
+        idMeal: '8',
+        strMeal: 'Seafood',
+        strMealThumb: 'https://www.themealdb.com/images/category/seafood.png',
+        price: '100'
+    },
+    {
+        idMeal: '9',
+        strMeal: 'Side',
+        strMealThumb: 'https://www.themealdb.com/images/category/side.png',
+        price: '100'
+    },
+    {
+        idMeal: '10',
+        strMeal: 'Starter',
+        strMealThumb: 'https://www.themealdb.com/images/category/starter.png',
+        price: '100'
+    },
+    {
+        idMeal: '11',
+        strMeal: 'Vegan',
+        strMealThumb: 'https://www.themealdb.com/images/category/vegan.png',
+        price: '100'
+    },
+    {
+        idMeal: '12',
+        strMeal: 'Vegetarian',
+        strMealThumb: 'https://www.themealdb.com/images/category/vegetarian.png',
+        price: '100'
+    },
+    {
+        idMeal: '13',
+        strMeal: 'Breakfast',
+        strMealThumb: 'https://www.themealdb.com/images/category/breakfast.png',
+        price: '100'
+    },
+    {
+        idMeal: '14',
+        strMeal: 'Goat',
+        strMealThumb: 'https://www.themealdb.com/images/category/goat.png',
+        price: '100'
+    },
+    {
+        strMeal: 'BeaverTails',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg',
+        idMeal: '52928'
+    },
+    {
+        strMeal: 'Breakfast Potatoes',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/1550441882.jpg',
+        idMeal: '52965'
+    },
+    {
+        strMeal: 'Canadian Butter Tarts',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg',
+        idMeal: '52923'
+    },
+    {
+        strMeal: 'Montreal Smoked Meat',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg',
+        idMeal: '52927'
+    },
+    {
+        strMeal: 'Nanaimo Bars',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg',
+        idMeal: '52924'
+    },
+    {
+        strMeal: 'Pate Chinois',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg',
+        idMeal: '52930'
+    },
+    {
+        strMeal: 'Pouding chomeur',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg',
+        idMeal: '52932'
+    },
+    {
+        strMeal: 'Poutine',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg',
+        idMeal: '52804'
+    },
+    {
+        strMeal: 'Rappie Pie',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg',
+        idMeal: '52933'
+    },
+    {
+        strMeal: 'Split Pea Soup',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg',
+        idMeal: '52925'
+    },
+    {
+        strMeal: 'Sugar Pie',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/yrstur1511816601.jpg',
+        idMeal: '52931'
+    },
+    {
+        strMeal: 'Timbits',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/txsupu1511815755.jpg',
+        idMeal: '52929'
+    },
+    {
+        strMeal: 'Tourtiere',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/ytpstt1511814614.jpg',
+        idMeal: '52926'
+    }
+]
